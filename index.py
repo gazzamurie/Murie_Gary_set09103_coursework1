03ea44e113cb891b6f596894541d1dff23c2a29e
Gary Murie <garymurie@Garys-MacBook-Air-2.local>
2017-10-15 22:45:37 +0100

    Got files uploading to a file, still to display them.

--- index.py
+++ index.py
@@ -1,20 +1,37 @@
-from flask import Flask, url_for, render_template
-import os
+from flask import Flask, url_for, render_template, request
+import os #Manipulates files
 from PIL import Image
 
 app = Flask(__name__)
 
-@app.route('/images/<filename>')
-def send_image(filename):
-    return send_images("static/static/", filename)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def home():
-    viewImages = os.listdir('static/static/')
-    print(viewImages)
-    return render_template("base.html", viewImages=viewImages)
+    #viewImages = os.listdir('static/images/')
+    return render_template("base.html")
 
+@app.route("/uploaded", methods=['POST','GET'])
+def uploaded():
 
+    if request.method == 'POST':
+        f = request.files['datafile']
+        f.save('static/images/upload.gif')
+        return "File Uploaded"
+    else:
+        page='''
+        <html>
+        <body>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <input type="file" name="datafile" accept="image/gif">
+            <input type="submit" value="send">
+        </form>
+        </body>
+        </html>
+        '''
+        return page, 200
+
+#Handles the error is user goes to link in our domain that doesnt exsist
 @app.errorhandler(404)
 def page_not_found(error):
     start = '<img src="'
